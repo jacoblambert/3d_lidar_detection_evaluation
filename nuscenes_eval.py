3f762037909becff15313f71aed0f06ffe86f388
baijun <jun.bai@vcarsystemp.com>
2023-10-13 18:08:54 +0800

    加入一些参数的传参简写

--- nuscenes_eval.py
+++ nuscenes_eval.py
@@ -4,15 +4,15 @@ from nuscenes_eval_core import NuScenesEval
 
 def parse_args():
     parser = argparse.ArgumentParser(description='arg parser')
-    parser.add_argument('--pred_labels', type=str, required=True,
+    parser.add_argument('--pred_labels','-p', type=str, required=True,
                         help='Prediction labels data path')
-    parser.add_argument('--gt_labels', type=str, required=True,
+    parser.add_argument('--gt_labels','-g', type=str, required=True,
                         help='Ground Truth labels data path')
-    parser.add_argument('--save_loc', type=str, required=True, help='Save location')
-    parser.add_argument('--format', type=str, default='class x y z l w h r score')
-    parser.add_argument('--classes', nargs='+', type=str,default=['car','pedestrian','cyclist'])
-    parser.add_argument('--max_range', type=float, default=0.0, help='max evaluation range')
-    parser.add_argument('--min_score', type=float, default=0.0, help='min detection score considered')
+    parser.add_argument('--save_loc','-s', type=str, required=True, help='Save location')
+    parser.add_argument('--format','-f', type=str, default='class x y z l w h r score')
+    parser.add_argument('--classes','-c', nargs='+', type=str,default=['car','pedestrian','cyclist'])
+    parser.add_argument('--max_range','-mr', type=float, default=0.0, help='max evaluation range')
+    parser.add_argument('--min_score','-ms', type=float, default=0.0, help='min detection score considered')
     args = parser.parse_args()
     return args
 
